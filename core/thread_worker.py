57e957067644748ce648cdf4f213f2edad6125ce
Ahmed <ahmedmahmo431@gmail.com>
2026-05-02 21:33:59 +0300

    adding thread worker module for concurrent pipeline avoiding GUI freezing

---
+++ core/thread_worker.py
@@ -0,0 +1,27 @@
+from PyQt5.QtCore import QThread, pyqtSignal
+from core.pipeline import pipeline as run_analysis
+
+class ThreadWorker(QThread):
+    finished = pyqtSignal(dict) # emits the result dict from pipeline
+    error = pyqtSignal(str)     # emits an error message string
+
+    def __init__(self, code_string, mode, case, sizes_range=None, manual_array=None):
+        super().__init__()
+        self.code_string   = code_string
+        self.mode          = mode
+        self.case          = case
+        self.sizes_range   = sizes_range if sizes_range else (128, 4096)
+        self.manual_array  = manual_array
+
+    def run(self):
+        try:
+            result = run_analysis(
+                self.code_string,
+                self.mode,
+                self.case,
+                sizes_range=self.sizes_range,
+                manual_array=self.manual_array
+            )
+            self.finished.emit(result)
+        except Exception as e:
+            self.error.emit(str(e))
